23ab59fb8a467a046e13368e891ca2c41e88fc1e
Nathaniel Aymer <n8@Nathaniels-MacBook-Pro.local>
2020-09-18 13:40:48 -0400

    Netlify Test

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,14 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+    <div className="Hero">
+      <div className="HeroGroup">
+         <h1>Re-Thinking <br /> Business Cards</h1>
+         <p>Share your Businesses and Social Media with a simple tap </p>
+         <Link to="/page-2/">Learn More</Link>
+     </div>
+   </div>
+ </div>
+ )    
 
 export default IndexPage
